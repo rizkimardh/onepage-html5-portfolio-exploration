042e70edea469606abfc8504bed81a1fb3be1f4e
Rizki <48363514+rizkimardh@users.noreply.github.com>
2022-05-25 17:57:00 +0700

    Add files via upload

---
+++ chills/assets/php/contact.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to = "rizkimrdth@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@lucifre.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:html/thanks.html");
+?>
